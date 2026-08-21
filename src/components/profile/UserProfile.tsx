785fea2325e8f0953cbe6850da323d7d0d27934c
ThibaultNGUYEN26 <thibault.nguyen26@gmail.com>
2026-08-21 23:43:33 +0200

    Restore spacious profile recipe grid

--- src/components/profile/UserProfile.tsx
+++ src/components/profile/UserProfile.tsx
@@ -429,7 +429,7 @@ export default function UserProfile({ userIdOverride }: { userIdOverride?: numbe
           )}
         </div>
       ) : (
-        <div className="grid grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-4 lg:gap-4">
+        <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
           {tabContent.map((r) => <RecipeCard key={r.slug} recipe={r} hideAuthor />)}
         </div>
       )}
